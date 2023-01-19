b0a01a416eb1eb3c3599c8ae0acf36d844227625
Kate Steed <sunfiresteed@yahoo.com>
2023-01-19 13:35:17 -0700

    server will now start when typing node server.js

--- routes/routes_index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const notesRouter = require('./routes_notes')
-
-const app = express();
-
-app.use('/notes', notesRouter);
-
-module.exports = app
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const fs = require('fs');
+const { v4 : uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile, writeToFile} = require('./helpers/fsUtils'); 
+
 const path = require('path');
-const api = require('./routes/routes_index')
 
 const app = express();
 const PORT = process.env.port || 3001;
@@ -9,7 +11,6 @@ const PORT = process.env.port || 3001;
 //Middleware for parsing JSON on urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -23,6 +24,33 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+// POST request for new notes
+app.post('/api/notes', (req, res) => {
+  const {title, content} = req.body;
+  
+  if (title && content) {
+    const newNote = {
+      title,
+      content,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNotes,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+})
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
